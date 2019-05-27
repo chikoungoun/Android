ddaa34c11ab837d365b5b2969161e2ce43e8dcc0
Chikoungoun <marwane.lotfi@gmail.com>
2019-05-27 14:06:29 +0000

    Making the code more robust with loader

---
+++ EarthQuake Report/EarthQuake Loaders/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.quakereport;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EarthquakeActivity extends AppCompatActivity implements LoaderCallbacks<List<Earthquake>> {
+
+    public static final String LOG_TAG = EarthquakeActivity.class.getName();
+
+    /**
+     * Constant value for the earthquake loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int EARTHQUAKE_LOADER_ID = 1;
+
+    private EarthQuakeAdapter adapter;
+
+    public static  final String USGS_REQUEST_URL ="https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+
+
+    /***
+     * Implementing Loaders functionality
+     *
+     */
+
+    @Override
+    public Loader<List<Earthquake>> onCreateLoader(int i,Bundle bundle) {
+        return new EarthquakeLoader(this,USGS_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Earthquake>> loader, List<Earthquake> data) {
+        adapter.clear();
+
+        if(data != null && !data.isEmpty()){
+            adapter.addAll(data);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Earthquake>> loader) {
+        adapter.clear();
+    }
+
+    /*
+            Loader End
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.earthquake_activity);
+
+
+
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+        // Create a new {@link ArrayAdapter} of earthquakes
+        adapter = new EarthQuakeAdapter(this, new ArrayList<Earthquake>());
+
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        earthquakeListView.setAdapter(adapter);
+
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Earthquake currentEarthquake = adapter.getItem(position);
+
+                // Convert the string into a URI object
+                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+
+
+                Intent i = new Intent(Intent.ACTION_VIEW,earthquakeUri);
+                startActivity(i);
+            }
+        });
+
+
+
+        LoaderManager loaderManager = getLoaderManager();
+
+        loaderManager.initLoader(EARTHQUAKE_LOADER_ID,null,this);
+
+
+
+    }
+
+
+}
---
+++ EarthQuake Report/EarthQuake Loaders/app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -0,0 +1,38 @@
+package com.example.android.quakereport;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = EarthquakeLoader.class.getName();
+
+    private String url;
+
+    public EarthquakeLoader(Context context, String u){
+        super(context);
+        url = u;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+
+
+    @Override
+    public List<Earthquake> loadInBackground() {
+        if(url == null){
+            return null;
+        }
+
+        List<Earthquake> earthquakes = QueryUtils.fetchEarthquakeData(url);
+        return  earthquakes;
+    }
+
+
+}
