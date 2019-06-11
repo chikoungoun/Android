fac6abe0de30ac9519a9daf85407f6e27f233c8d
Chikoungoun <marwane.lotfi@gmail.com>
2019-06-11 21:20:36 +0100

    displaying the database content int the cataogActivity

--- Pets Shelter/app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ Pets Shelter/app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -62,23 +62,7 @@ public class CatalogActivity extends AppCompatActivity {
 
     }
 
-    // Add a pet to the database
-    private void insertPet(){
 
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-        ContentValues values = new ContentValues();
-
-        values.put(PetEntry.COLUMN_PET_NAME,"Toto");
-        values.put(PetEntry.COLUMN_PET_BREED,"Terrier");
-        values.put(PetEntry.COLUMN_PET_GENDER,PetEntry.GENDER_MALE);
-        values.put(PetEntry.COLUMN_PET_WEIGHT,7);
-
-
-        long newRowId = db.insert(PetEntry.TABLE_NAME,null,values);
-        Log.e("CatalogActivity","New row ID : "+newRowId);
-
-
-    }
 
 
     // Inflate the catalog menu
@@ -123,15 +107,89 @@ public class CatalogActivity extends AppCompatActivity {
         // Create and open a databse
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
-        // get a Cursor containing all rows from the pets table
-        Cursor cursor = db.rawQuery("SELECT * FROM "+ PetEntry.TABLE_NAME, null);
+        String[] projection = {
+                PetEntry._ID,
+                PetEntry.COLUMN_PET_NAME,
+                PetEntry.COLUMN_PET_BREED,
+                PetEntry.COLUMN_PET_GENDER,
+                PetEntry.COLUMN_PET_WEIGHT
+        };
+
+        // instanciate a new cursor
+        Cursor cursor = db.query(
+                PetEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+
 
         try{
-            TextView displayView = (TextView) findViewById(R.id.text_view_pet);
-            displayView.setText("Number of rows in pets databse table:"+ cursor.getCount());
+
+            // Displaying the number of observations and the structure in the activity_catalog.xml
+            displayView.setText(" The set table contains "+ cursor.getCount()+" pets.\n\n");
+            displayView.append(PetEntry._ID+" - "+
+                    PetEntry.COLUMN_PET_NAME+" - "+
+                    PetEntry.COLUMN_PET_BREED+" - "+
+                    PetEntry.COLUMN_PET_GENDER+" - "+
+                    PetEntry.COLUMN_PET_WEIGHT+"\n");
+
+            // Index of each column in the cursor
+            int idColumnIndex  = cursor.getColumnIndex(PetEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
+            int breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
+            int genderColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_GENDER);
+            int weightColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT);
+
+            //iterate through the rows from the cursor
+            while(cursor.moveToNext()){
+
+                // get the value from each index
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentName = cursor.getString(nameColumnIndex);
+                String currentBreed = cursor.getString(breedColumnIndex);
+                int currentGender = cursor.getInt(genderColumnIndex);
+                int currentWeight = cursor.getInt(weightColumnIndex);
+
+                //Display the values of each row in the textView
+                displayView.append("\n"+currentID+" - "+
+                        currentName+" - "+
+                        currentBreed+" - "+
+                        currentGender+" - "+
+                        currentWeight);
+
+
+            }
+
+
         }finally {
+            // closing the cursor
             cursor.close();
 
         }
+
+        }
+
+    // Add a pet to the database
+    private void insertPet(){
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(PetEntry.COLUMN_PET_NAME,"Toto");
+        values.put(PetEntry.COLUMN_PET_BREED,"Terrier");
+        values.put(PetEntry.COLUMN_PET_GENDER,PetEntry.GENDER_MALE);
+        values.put(PetEntry.COLUMN_PET_WEIGHT,7);
+
+
+        long newRowId = db.insert(PetEntry.TABLE_NAME,null,values);
+        Log.e("CatalogActivity","New row ID : "+newRowId);
+
+
     }
+
 }
