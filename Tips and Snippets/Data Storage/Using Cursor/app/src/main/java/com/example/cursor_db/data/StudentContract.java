b88e58e984e82b1311c9d027e1bb05bb508048a0
Chikoungoun <marwane.lotfi@gmail.com>
2019-06-12 12:37:01 +0100

    Contract class + DB Helper
    -

---
+++ Tips and Snippets/Data Storage/Using Cursor/app/src/main/java/com/example/cursor_db/data/StudentContract.java
@@ -0,0 +1,25 @@
+package com.example.cursor_db.data;
+
+import android.provider.BaseColumns;
+
+// Defining the constants for the
+
+public final class StudentContract {
+
+    public StudentContract() {
+    }
+
+    public class StudentEntry implements BaseColumns{
+
+        // Table name
+        public static final String TABLE_NAME = "students";
+
+        // Table column names (create a unique ID)
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_STUDENT_FIRSTNAME = "First name";
+        public static final String COLUMN_STUDENT_LASTNAME = "Last name";
+        public static final String COLUMN_STUDENT_AGE = "Age";
+
+
+    }
+}
---
+++ Tips and Snippets/Data Storage/Using Cursor/app/src/main/java/com/example/cursor_db/data/StudentDBHelper.java
@@ -0,0 +1,41 @@
+package com.example.cursor_db.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.cursor_db.data.StudentContract.StudentEntry;
+
+
+// Creating a database and managing the version
+
+
+public class StudentDBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "school";
+
+    public static final int DATABASE_VERSION = 1;
+
+    public StudentDBHelper(Context context) {
+        super(context, DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        //Creating the database table
+        String SQL_CREATE_TABLE = "CREATE TABLE "+StudentEntry.TABLE_NAME+"("
+                +StudentEntry._ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "
+                +StudentEntry.COLUMN_STUDENT_FIRSTNAME+" TEXT, "
+                +StudentEntry.COLUMN_STUDENT_LASTNAME+" TEXT NOT NULL "
+                +StudentEntry.COLUMN_STUDENT_AGE+" INTEGER);";
+
+        db.execSQL(SQL_CREATE_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
