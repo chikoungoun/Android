32a30ebd3d7ed8f2dba2b0c94dbd98ddb03cb47e
Chikoungoun <marwane.lotfi@gmail.com>
2019-06-24 16:19:30 +0100

    Réussite d'extraire les données d'un fichier JSON
    - Extraction d'un fichier JSON depuis internet
    - Lecture du contenu
    - Affichage dans une ListView

--- Whats_In/main/java/com/example/cuturehttp/MainActivity.java
+++ Whats_In/main/java/com/example/cuturehttp/MainActivity.java
@@ -1,26 +1,67 @@
 package com.example.cuturehttp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SpectacleAdapter adapter;
+
+    public static final String SPEC_JSON="https://raw.githubusercontent.com/chikoungoun/Android/master/Whats_In/main/tnm5_records";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Extraire les données du JSON
-        ArrayList<Spectacle> spectacles = QueryUtils.extractSpectacles();
-
         ListView listView = (ListView)findViewById(R.id.list);
 
-        SpectacleAdapter adapter = new SpectacleAdapter(this,spectacles);
+        adapter = new SpectacleAdapter(this,new ArrayList<Spectacle>());
 
         listView.setAdapter(adapter);
+
+        SpectacleAsyncTask task = new SpectacleAsyncTask();
+        task.execute(SPEC_JSON);
+
+    }
+
+    // Il n'est pas possible d'effectuer des opérations networking sur le main Thread, de peur
+    // de faire grandement ralentir le système. On utilise un thread parallèle
+
+    private class SpectacleAsyncTask extends AsyncTask<String,Void, List<Spectacle>> {
+
+
+        @Override
+        protected List<Spectacle> doInBackground(String... urls) {
+
+            if(urls.length < 1 || urls[0] == null){
+                return null;
+            }
+
+            //Extraire les données du json
+            List result = QueryUtils.fetchSpectacleData(urls[0]);
+
+            return result;
+        }
+
+
+
+        @Override
+        protected void onPostExecute(List<Spectacle> data) {
+
+            // Nettoyer l'adapter des données précédentes
+            adapter.clear();
+
+
+            if(data != null && !data.isEmpty()){
+                adapter.addAll(data);
+            }
+        }
     }
 }
--- Whats_In/main/java/com/example/cuturehttp/QueryUtils.java
+++ Whats_In/main/java/com/example/cuturehttp/QueryUtils.java
@@ -1,12 +1,22 @@
 package com.example.cuturehttp;
 
+import android.text.TextUtils;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 
 public class QueryUtils {
 
@@ -20,15 +30,93 @@ public class QueryUtils {
     private QueryUtils() {
     }
 
-    //Extraire un array d'objet depuis le JSON
-    public static ArrayList<Spectacle> extractSpectacles(){
+    // Créer et vérifier le lien URL
+    private static URL createURL(String stringUrl){
+        URL url = null;
+
+        try{
+            url = new URL(stringUrl);
+        }catch (MalformedURLException e){
+            Log.e("QueryUtils","Problem building the URL",e);
+        }
+
+        return url;
+    }
+
+    // Effectuer la requête HTTP pour récupérer les données
+    private static String makeHttpRequest(URL url) throws IOException{
+
+        String jsonResponse = "";
+
+        // Si l'URL est null
+        if(url == null){
+            return  jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try{
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Si le retour URL fonctionne bien (code 200), parsing de la réponse
+            if(urlConnection.getResponseCode() == 200){
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            }else{
+                Log.e("QueryUtils","Error Response code : "+urlConnection.getResponseCode());
+            }
+        }catch (IOException e){
+            Log.e("QueryUtils","Problème pour récupérer le fichier JSON",e);
+        }finally {
+            if(urlConnection != null){
+                urlConnection.disconnect();
+            }
+
+            if(inputStream != null){
+                // fermer le inputstream peut générer une IOException, c'est pour quoi on la "jette"
+                inputStream.close();
+            }
+        }
+
+        return jsonResponse;
+    }
+
+    // Convertir le InputStream en String contenant la réponse JSON
+    private static String readFromStream(InputStream inputStream) throws IOException{
+
+        StringBuilder output = new StringBuilder();
+        if(inputStream != null){
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null){
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+
+        return output.toString();
+    }
+
+    //Extraire dans un array d'objet à partir du parsing du JSON
+    public static ArrayList<Spectacle> extractFromJSON(String specJSON){
+
+        //si le texte JSON est null
+        if(TextUtils.isEmpty(specJSON)){
+            return null;
+        }
 
         ArrayList<Spectacle> spectacles = new ArrayList<Spectacle>();
 
         try{
-            //JSONObject jobject = new JSONObject(JSON_FILE);
 
-            JSONArray jsonArray = new JSONArray(JSON_FILE);
+
+            JSONArray jsonArray = new JSONArray(specJSON);
 
             for(int i=0;i<jsonArray.length();i++){
 
@@ -47,6 +135,25 @@ public class QueryUtils {
             Log.e("QueryUtils","Problems with parsing the JSON file",e);
 
         }
+
+        return spectacles;
+    }
+
+    // Fonction regroupement toutes les fonctionnalités
+    public static List<Spectacle> fetchSpectacleData(String requestUrl){
+
+        // créer l'objet URL
+        URL url = createURL(requestUrl);
+
+        // Effectuer requête Http
+        String jsonResponse = null;
+        try{
+            jsonResponse = makeHttpRequest(url);
+        }catch (IOException e){
+            Log.e("fetchSpectacleData","Problème de requête HTTP",e);
+        }
+
+        List<Spectacle> spectacles = extractFromJSON(jsonResponse);
 
         return spectacles;
     }
