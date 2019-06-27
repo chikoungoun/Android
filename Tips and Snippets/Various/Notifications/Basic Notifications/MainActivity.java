831baee4465371f4d3fcb23f867dceb45baae31e
Chikoungoun <marwane.lotfi@gmail.com>
2019-06-27 16:28:04 +0100

    Basic Notification for += Android 8.0

---
+++ Tips and Snippets/Various/Notifications/Basic Notifications/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.functionalities;
+
+import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/*
+
+        Listening on android views to check if they were clicked
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String CHANNEL_ID = "my Channel";
+    private final int NOTIFICATION_ID = 100;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNotification();
+            }
+        });
+
+    }
+
+    public void createNotification(){
+
+        createNotificationChannel();
+
+        Intent intent = new Intent(this, NotificationReceiver.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this,(int)System.currentTimeMillis(),intent,0);
+
+        // Build Notification
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this,CHANNEL_ID)
+                .setContentTitle("New Notif from Chunga")
+                .setContentText("Subject")
+                .setSmallIcon(R.drawable.ic_launcher_foreground);
+
+        notification.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID,notification.build());
+    }
+
+
+
+    private void createNotificationChannel(){
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "Personal Notifications";
+            String description = "Include all the personal notifications";
+
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,name,importance);
+
+            notificationChannel.setDescription(description);
+
+            NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+            notificationManager.createNotificationChannel(notificationChannel);
+
+
+
+        }
+
+    }
+
+
+
+}
