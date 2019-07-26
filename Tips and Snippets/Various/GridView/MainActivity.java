093974d646fddb6dfd5667c24fd0c187376fbdb7
Chikoungoun <marwane.lotfi@gmail.com>
2019-07-26 11:21:17 +0100

    Basic GridView

---
+++ Tips and Snippets/Various/GridView/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.mygridview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
+
+    static final String[] letters = new String[] {
+            "A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridView =(GridView) findViewById(R.id.gridview);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,letters);
+
+        gridView.setAdapter(adapter);
+
+
+
+    }
+}
