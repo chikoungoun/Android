8350bddd36470a807ec522d9bfa62a40964514a9
Chikoungoun <marwane.lotfi@gmail.com>
2019-08-01 14:34:53 +0100

    Basic Collapsing Toolbar Code snippet

---
+++ Tips and Snippets/Various/CollapsingToolbar/java/com/example/mytoolbar/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mytoolbar;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("BOUYAKA");
+
+    }
+}
