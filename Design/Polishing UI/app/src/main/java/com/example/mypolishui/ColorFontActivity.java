1eab2d1efd109c020c4675057d4173872e92109b
Chikoungoun <marwane.lotfi@gmail.com>
2019-08-14 12:25:06 +0100

    Polishing UI upload project

---
+++ Design/Polishing UI/app/src/main/java/com/example/mypolishui/ColorFontActivity.java
@@ -0,0 +1,17 @@
+package com.example.mypolishui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mypolishui.R;
+
+
+public class ColorFontActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.color_font_activity);
+
+    }
+}
---
+++ Design/Polishing UI/app/src/main/java/com/example/mypolishui/ResponsiveLayoutActivity.java
@@ -0,0 +1,18 @@
+package com.example.mypolishui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mypolishui.R;
+
+
+public class ResponsiveLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.responsive_activity);
+
+    }
+
+}
