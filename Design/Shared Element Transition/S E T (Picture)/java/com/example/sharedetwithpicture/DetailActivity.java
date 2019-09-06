babafc4186a270f311553eb42d8fffe922340f83
Chikoungoun <marwane.lotfi@gmail.com>
2019-09-06 18:37:44 +0100

    Adding a fading effect to avoid flashing

--- Design/Shared Element Transition/S E T (Picture)/java/com/example/sharedetwithpicture/DetailActivity.java
+++ Design/Shared Element Transition/S E T (Picture)/java/com/example/sharedetwithpicture/DetailActivity.java
@@ -2,6 +2,8 @@ package com.example.sharedetwithpicture;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Fade;
+import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         setTitle("Detail Activity");
+
+        // Adding a fading effect and avoid flashing effect
+        Fade fade = new Fade();
+        View decor = getWindow().getDecorView();
+        fade.excludeTarget(decor.findViewById(R.id.action_bar_container),true);
+        fade.excludeTarget(android.R.id.statusBarBackground,true);
+        fade.excludeTarget(android.R.id.navigationBarBackground,true);
+
+        getWindow().setEnterTransition(fade);
+        getWindow().setExitTransition(fade);
     }
 }
--- Design/Shared Element Transition/S E T (Picture)/java/com/example/sharedetwithpicture/MainActivity.java
+++ Design/Shared Element Transition/S E T (Picture)/java/com/example/sharedetwithpicture/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Fade;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setTitle("Main Activity");
+
+        // Adding a fading effect and avoid flashing effect
+        Fade fade = new Fade();
+        View decor = getWindow().getDecorView();
+        fade.excludeTarget(decor.findViewById(R.id.action_bar_container),true);
+        fade.excludeTarget(android.R.id.statusBarBackground,true);
+        fade.excludeTarget(android.R.id.navigationBarBackground,true);
+
+        getWindow().setEnterTransition(fade);
+        getWindow().setExitTransition(fade);
 
         final ImageView imageView = findViewById(R.id.img_act1);
 
