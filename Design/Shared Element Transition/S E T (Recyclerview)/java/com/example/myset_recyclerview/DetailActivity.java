a9a586438e3f0227fab57c23ecdf17016e380807
Chikoungoun <marwane.lotfi@gmail.com>
2019-09-08 13:56:14 +0100

    Implementing the onClick method and using a stupid example

---
+++ Design/Shared Element Transition/S E T (Recyclerview)/java/com/example/myset_recyclerview/DetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.myset_recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
--- Design/Shared Element Transition/S E T (Recyclerview)/java/com/example/myset_recyclerview/MainActivity.java
+++ Design/Shared Element Transition/S E T (Recyclerview)/java/com/example/myset_recyclerview/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.myset_recyclerview;
 
+import android.content.Intent;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ContactsAdapter.OnItemClickListener {
 
     ArrayList<Contact> contacts;
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +26,31 @@ public class MainActivity extends AppCompatActivity {
 
         contacts = Contact.createContactList(20);
 
+        imageView = findViewById(R.id.images);
+
         // Create adapter passing in the sample user data
         ContactsAdapter adapter = new ContactsAdapter(contacts);
         // Attach the adapter to the recyclerview to populate items
         rvContacts.setAdapter(adapter);
         // Set layout manager to position the items
         rvContacts.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+
+    }
+
+    @Override
+    public void onItemClick(int position) {
+
+        Intent intent = new Intent(this,DetailActivity.class);
+        Contact clickedItem = contacts.get(position);
+
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                MainActivity.this,
+                imageView,
+                ViewCompat.getTransitionName(imageView));
+        startActivity(intent,options.toBundle());
+
     }
 }
