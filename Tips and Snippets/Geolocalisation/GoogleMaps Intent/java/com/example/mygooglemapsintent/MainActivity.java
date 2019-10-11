9e85a1301ef976dbfdc0ec09850eb82aab6239e9
Chikoungoun <marwane.lotfi@gmail.com>
2019-10-11 17:25:19 +0100

    Sending geo coordinates to Google Maps through Intent

---
+++ Tips and Snippets/Geolocalisation/GoogleMaps Intent/java/com/example/mygooglemapsintent/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.mygooglemapsintent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Create a Uri from an intent string. Use the result to create an Intent.
+                Uri gmmIntentUri = Uri.parse("geo:33.592827, -7.631955");
+
+
+
+                // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                // Make the Intent explicit by setting the Google Maps package
+                mapIntent.setPackage("com.google.android.apps.maps");
+
+                // Attempt to start an activity that can handle the Intent
+                startActivity(mapIntent);
+            }
+        });
+
+
+
+    }
+}
