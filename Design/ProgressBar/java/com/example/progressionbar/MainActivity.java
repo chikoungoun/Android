5972efafe225dc655162ab36e8c04ab0445331e9
Chikoungoun <marwane.lotfi@gmail.com>
2019-11-20 12:31:25 +0100

    Basic ProgressBar

---
+++ Design/ProgressBar/java/com/example/progressionbar/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.progressionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private int progressStatus = 0;
+    private TextView textView;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
+        textView = findViewById(R.id.textView);
+
+
+
+    }
+}
