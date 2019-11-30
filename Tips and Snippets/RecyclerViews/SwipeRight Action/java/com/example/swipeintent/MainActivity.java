7ca7d19e5a2f269fa64157da6876fca87ed2d958
Chikoungoun <marwane.lotfi@gmail.com>
2019-11-30 18:16:22 +0100

    Settin gup a colored background while swiping

--- Tips and Snippets/RecyclerViews/SwipeRight Action/java/com/example/swipeintent/MainActivity.java
+++ Tips and Snippets/RecyclerViews/SwipeRight Action/java/com/example/swipeintent/MainActivity.java
@@ -7,8 +7,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -53,10 +59,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(call_button);
 
                 rvContacts.setAdapter(adapter);
-
             }
 
+            @Override
+            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
 
+                View itemView = viewHolder.itemView;
+                int itemHeight = itemView.getHeight();
+                final ColorDrawable background = new ColorDrawable(Color.RED);
+
+                background.setBounds(0,itemView.getTop(),itemView.getLeft() + (int) dX, itemView.getBottom());
+
+                background.draw(c);
+            }
         }).attachToRecyclerView(rvContacts);
     }
 }
