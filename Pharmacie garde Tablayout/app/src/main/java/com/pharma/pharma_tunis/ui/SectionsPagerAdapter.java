bd11fc016efc07baca8c56f146391a891716f44c
Chikoungoun <marwane.lotfi@gmail.com>
2020-02-16 14:07:36 +0100

    Adapting the Tablayout format to my code

---
+++ Pharmacie garde Tablayout/app/src/main/java/com/pharma/pharma_tunis/ui/SectionsPagerAdapter.java
@@ -0,0 +1,49 @@
+package com.pharma.pharma_tunis.ui;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.pharma.pharma_tunis.R;
+import com.pharma.pharma_tunis.Tab1;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                Tab1 tab1 = new Tab1();
+                return tab1;
+            case 1:
+                Tab2 tab2 = new Tab2();
+                return tab2;
+        }
+
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
